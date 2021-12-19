4e34dd66a11d88753e5d25617fc2b0fa756710fc
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2021-12-19 11:14:27 +0100

    ggrepel

--- work/oecd.r
+++ work/oecd.r
@@ -7,6 +7,7 @@ library(ggrepel)
 library(ggh4x)
 library(glue)
 library(tsibble)
+library(ofce)
 
 eoid <- OECD::get_datasets() |>
   filter(str_detect(id, "^EO")) |>
@@ -27,27 +28,27 @@ oecd <- oecd_raw %>%
   pivot_wider(names_from=c(VARIABLE), values_from=ObsValue) %>%
   as_tsibble(key=pays, index=year) %>%
   group_by_key() %>%
-  mutate(dette = if(all(is.na(GNFLQ))) GGFLMQ else GNFLQ,
-         dette = dette-dette[which(year==2007)]) |>
-  mutate(pays = factor(pays, c("EA17","GBR","USA", ez)))
+  mutate(dette = if(all(is.na(GNFLQ))) GGFLMQ else GNFLQ, # si pas maastritcht, liabilities
+         dette = dette-dette[which(year==2007)]) |> # calcul de l'écart
+  mutate(pays = factor(pays, c(ez,"EA17","GBR","USA"))) # pour mettre dans l'ordre et couvrir les gris
 
-breaks <- c(start_y, seq(2000,2030,5) ) |> sort() |> keep(~between(.x, start_y, end_y))
-colors <- c(set_names(ofce_palette(3), c("EA17","GBR","USA")), set_names(rep("grey70", length(ez)), ez))
+breaks <- c(start_y, seq(2000,2030,5) ) |> sort() |> keep(~between(.x, start_y, end_y)) # manual breaks pour plus de finesse
+colors <- c(set_names(rep("grey80", length(ez)), ez), set_names(ofce_palette(3), c("EA17","GBR","USA"))) # couleurs
+
 dette <- (ggplot(oecd %>% filter(between(year,start_y,end_y)),
-                 aes(x=year, y=dette, group=pays))+
-            geom_line(lwd=0.25, show.legend = FALSE,  color = "grey70")+
-            geom_line(data=~filter(.x, pays%in%c("EA17","GBR","USA")),
-                      size=0.5,
-                      mapping=aes(color=pays))+
+                 aes(x=year, y=dette, group=pays, color=pays))+
+            geom_line(data= ~.x, #~filter(.x, pays%in%c("EA17","GBR","USA")) alternative
+                      size=0.5)+
+            geom_line(lwd=0.25, show.legend = FALSE)+
             geom_text_repel(data=~mutate(
-              .x,
+              filter(.x,pays%in%c("EA17","GBR","USA")),
               label=if_else(pays%in%c("EA17","GBR","USA")&year==2015,
                             as.character(pays), "")),
               aes(label=label, color=label),
-              size=3,
+              size=3,force_pull = 0, nudge_y = 2.5,
               segment.size = 0.1, min.segment.length=0,
               show.legend = FALSE)+
-            scale_color_manual(values = colors )+
+            scale_color_manual(values = colors)+
             scale_x_continuous(name="", minor_breaks = seq(start_y, end_y, 1), breaks=breaks, guide="axis_minor")+
             scale_y_continuous(name="")+
             guides(color="none")+
@@ -58,4 +59,4 @@ dette <- (ggplot(oecd %>% filter(between(year,start_y,end_y)),
   add_label_unit(ylabel="% du PIB") |>
   add_logo_ofce()
 
-ggsave(".work/dette.svg", dette, width=18, height=14, unit="cm")
+ggsave("work/dette.svg", dette, width=18, height=14, unit="cm")
