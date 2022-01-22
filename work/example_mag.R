37c57cbb772102134abab79b52537595532d6b9a
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2022-01-22 16:25:35 +0100

    Update example_mag.R

--- work/example_mag.R
+++ work/example_mag.R
@@ -7,7 +7,7 @@ library(lubridate)
 library(eurostat)
 # devtools::install_github("AllanCameron/geomtextpath", quiet = TRUE)
 library(geomtextpath)
-
+library(ggh4x)
 load("work/IC_DR_ECFIN.Rda")
 v_source <- ""
 # un calcul un peu complqué pour les breaks, faudra le mettre à sa place dans le package ofce
@@ -53,12 +53,14 @@ data <- IC_DR_ECFIN |>
          name = if_else(str_detect(name, "Zone euro"), "Zone euro", name))
 
 #  et ensuite j'utilise le package geomtextpath qui est trop cool
+# (bien installer la version github, il y a des fonctionnalités en plus, le rendu est meilleur)
 #  les noms sont sur la courbe et ils la suivent, en couleur
 #  et d'un coup de baguette magique
 #
-g_IC_DR_2<- ggplot(data, aes(x=period, y=value, col = name)) +
+g_IC_DR_2<- ggplot(data, aes(x=period, y=value, col = name, group=name)) +
   # geom_line(key_glyph="timeseries")+ # option 1
-  geom_textline(aes(label=name), size = 2.5, padding=unit(1, "mm"), linewidth=0.5, text_smoothing = 30, show.legend = FALSE)+
+  geom_line(size = 0.5, alpha=0.25, show.legend = FALSE)+ # permet de garder la trace sous le texte (accessoire)
+  geom_textline(aes(label=name), size = 3, padding=unit(1, "mm"), linewidth=0.5, text_smoothing = 30, show.legend = FALSE)+
   labs( x = NULL, y = NULL,  color = NULL , # tu peux tout mettre dans labs NULL ne garde pas l'espace pour le texte
         title="Indice de difficultés de recrutement",
         caption=str_c("Source:  ", v_source, ", DG ECFIN",
@@ -67,7 +69,7 @@ g_IC_DR_2<- ggplot(data, aes(x=period, y=value, col = name)) +
   scale_color_manual(values = c( ofce_palette(4), "gray15"))+
   theme_ofce()+
   scale_y_continuous(minor_breaks = minor_breaks_unity, guide="axis_minor")+ # ca met des breaks mineurs (ggh4x)
-  # scale_x_date(breaks = bb , date_minor_breaks = "1 year", guide="axis_minor", date_labels = "%Y")
-  scale_x_date(date_breaks = "5 years" , date_minor_breaks = "1 year", guide="axis_minor", date_labels = "%Y") # alternative pas mal aussi
+  scale_x_date(breaks = bb , date_minor_breaks = "1 year", guide="axis_minor", date_labels = "%Y")
+  # scale_x_date(date_breaks = "5 years" , date_minor_breaks = "1 year", guide="axis_minor", date_labels = "%Y") # alternative pas mal aussi
 
 g_IC_DR_2
