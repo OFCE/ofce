8e7fa7d74127e6b994d8988e5476379987fd436a
PaulMalliet <paul.malliet@sciencespo.fr>
2022-02-19 17:54:49 +0100

    Update colors.r
    functions for palette on countries.
    
    Ar

--- R/colors.r
+++ R/colors.r
@@ -1,8 +1,8 @@
-#' Palettes de couleur pour la norme de la revue OFCE
+#' Palettes de couleurs pour la norme de la revue OFCE
 #'
-#' @param ncolor integer, nombre de couleur
+#' @param ncolor integer, nombre de couleurs
 #'
-#' @return une palette de couleur
+#' @return une palette de couleurs (format HEX)
 #' @export
 #'
 ofce_palette <- function(ncolor=2) {
@@ -24,4 +24,23 @@ ofce_palette <- function(ncolor=2) {
     # Bleu, Jaune, Rouge, Vert, Violet
     return(c("#005DA4", "#F59C00", "#C51315", "#008D36", "#9900CC"))
   }
+}
+
+#' Palettes de couleur associées aux pays pour la norme de la revue OFCE
+#'
+#' @param list_iso3 chr, une liste de nom de pays
+#'
+#' @return une palette de couleur (format HEX)
+#' @export
+#'
+#'
+ofce_palette_pays <- function(list_iso3 = list_iso.full) {
+
+  list_iso.full <- read.csv("work/palette/palette_OFCE.csv", sep = ";")[["ISO"]]
+
+  dat <- read.csv("work/palette/palette_OFCE.csv", sep = ";") |>
+    dplyr::filter(ISO %in% list_iso3) |>
+    dplyr::arrange(match(ISO, list_iso3))
+
+  return(dat$HEX)
 }
