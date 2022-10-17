2c4b19733ee4afebb11352b1b2d02b7f11850d3b
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2022-10-17 08:49:54 +0200

    Update installer Stone Sans.R

--- work/installer Stone Sans.R
+++ work/installer Stone Sans.R
@@ -3,8 +3,9 @@
 # Il faut d'abord installer Stone Sans à partir des fichiers dans le dossier stone-sans-cufonfonts
 #
 # Ensuite il faut installer cette version de ce package
+
 remotes::install_version("Rttf2pt1", version = "1.3.8")
 extrafont::font_import(pattern="Stone",recursive = TRUE, prompt=FALSE)
 extrafont::loadfonts(device="win")
 
-# vérifier que ragg est le moteur graphique (global options/R général/Graphics)
+# vérifier que agg est le moteur graphique (global options/R général/Graphics sélectionner AGG)
