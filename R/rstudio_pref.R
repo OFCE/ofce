779d2a8afddc24ff0f534aa75d79b3a8e3739fed
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2023-09-07 18:15:34 +0200

    prefs

--- R/rstudio_pref.R
+++ R/rstudio_pref.R
@@ -1,33 +1,35 @@
 #' Reset RStudio prefs
 #'
+#' Principalement, pas d'enregistrement de l'environnement, pipe natif, ragg en backend graphique
+#'
 #' @return NULL
 #' @importFrom rstudioapi writeRStudioPreference
 #' @export
 #'
 #' @examples
-#' setOFCERStudio()
-#' 
+#' if(interactive()) setOFCERStudio()
+#'
 setOFCERStudio <- function() {
-  writeRStudioPreference("jobs_tab_visibility", "shown")
-  writeRStudioPreference("initial_working_directory", "~")
-  writeRStudioPreference("posix_terminal_shell", "bash")
-  writeRStudioPreference("save_workspace", "never")
-  writeRStudioPreference("load_workspace", FALSE)
-  writeRStudioPreference("editor_theme", "iPlastic")
-  writeRStudioPreference("insert_native_pipe_operator", TRUE)
-  writeRStudioPreference("highlight_selected_line", TRUE)
-  writeRStudioPreference("show_margin", FALSE)
-  writeRStudioPreference("highlight_r_function_calls", TRUE)
-  writeRStudioPreference("rainbow_parentheses", TRUE)
-  writeRStudioPreference("spelling_dictionary_language", "fr_FR")
-  writeRStudioPreference("save_files_before_build", TRUE)
-  writeRStudioPreference("shiny_viewer_type", "pane")
-  writeRStudioPreference("visual_markdown_editing_font_size_points", 12)
-  writeRStudioPreference("graphics_backend", "ragg")
-  writeRStudioPreference("doc_outline_show", "sections_and_chunks")
-  writeRStudioPreference("visual_markdown_editing_max_content_width", 1200)
-  writeRStudioPreference("default_project_location", "~")
-  writeRStudioPreference("syntax_color_console", TRUE)
-  writeRStudioPreference("rmd_viewer_type", "pane")
-  writeRStudioPreference("load_workspace", FALSE )
+  rstudioapi::writeRStudioPreference("jobs_tab_visibility", "shown")
+  rstudioapi::writeRStudioPreference("initial_working_directory", "~")
+  rstudioapi::writeRStudioPreference("posix_terminal_shell", "bash")
+  rstudioapi::writeRStudioPreference("save_workspace", "never")
+  rstudioapi::writeRStudioPreference("load_workspace", FALSE)
+  rstudioapi::writeRStudioPreference("editor_theme", "iPlastic")
+  rstudioapi::writeRStudioPreference("insert_native_pipe_operator", TRUE)
+  rstudioapi::writeRStudioPreference("highlight_selected_line", TRUE)
+  rstudioapi::writeRStudioPreference("show_margin", FALSE)
+  rstudioapi::writeRStudioPreference("highlight_r_function_calls", TRUE)
+  rstudioapi::writeRStudioPreference("rainbow_parentheses", TRUE)
+  rstudioapi::writeRStudioPreference("spelling_dictionary_language", "fr_FR")
+  rstudioapi::writeRStudioPreference("save_files_before_build", TRUE)
+  rstudioapi::writeRStudioPreference("shiny_viewer_type", "pane")
+  rstudioapi::writeRStudioPreference("visual_markdown_editing_font_size_points", 12L)
+  rstudioapi::writeRStudioPreference("graphics_backend", "ragg")
+  rstudioapi::writeRStudioPreference("doc_outline_show", "sections_and_chunks")
+  rstudioapi::writeRStudioPreference("visual_markdown_editing_max_content_width", 900L)
+  rstudioapi::writeRStudioPreference("default_project_location", "~")
+  rstudioapi::writeRStudioPreference("syntax_color_console", TRUE)
+  rstudioapi::writeRStudioPreference("rmd_viewer_type", "pane")
+  rstudioapi::writeRStudioPreference("load_workspace", FALSE )
 }
