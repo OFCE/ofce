1f20c660a010739a7a90d86992800e2d2c6b1bc1
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2023-10-03 09:40:54 +0200

    fonts ok

--- R/zzz.r
+++ R/zzz.r
@@ -7,21 +7,21 @@
   )
   toset <- !(names(op.ofce) %in% names(op))
   if (any(toset)) options(op.ofce[toset])
-  if(curl::has_internet()) {
-    if(!any(sysfonts::font_files()$family |> stringr::str_detect("Open Sans"))) {
-      sysfonts::font_add_google("Open Sans")
-      cli::cli_alert_info("Open Sans installé")
-    }
-    # # Verifie que les fonts de la famille "Roboto" sont installées
-    # if (gdtools::font_family_exists(font_family = "Roboto") == FALSE){
-    #   sysfonts::font_add_google("Roboto", "Roboto")
-    #   cli::cli_alert_info("Roboto installé")
-    # }
 
-    # Verifie que les fonts de la famille "Nunito" sont installées
-    if (gdtools::font_family_exists(font_family = "Nunito") == FALSE){
-      sysfonts::font_add_google("Nunito", "Nunito")
-      cli::cli_alert_info("Nunito installé")
-    }
+  fonts_dir <- system.file("fonts", package= "ofce")
+  sysfonts::font_add(
+    "Open Sans",
+    regular  = stringr::str_c(fonts_dir, "/OpenSans/OpenSans-VariableFont_wdth,wght.ttf"),
+    italic = stringr::str_c(fonts_dir, "/OpenSans/OpenSans-Italic-VariableFont_wdth,wght.ttf"))
+
+  sysfonts::font_add(
+    "Nunito",
+    regular  = stringr::str_c(fonts_dir, "/Nunito/Nunito-VariableFont_wght.ttf"),
+    italic = stringr::str_c(fonts_dir, "/Nunito/Nunito-Italic-VariableFont_wght.ttf"))
+
+  sysfonts::font_add(
+    "Roboto",
+    regular  = stringr::str_c(fonts_dir, "/Roboto/Roboto-Medium.ttf"),
+    italic = stringr::str_c(fonts_dir, "/Roboto/Roboto-Italic.ttf"))
+
   }
-}
--- work/test ggplot.R
+++ work/test ggplot.R
@@ -1,7 +1,8 @@
 library(ggplot2)
 library(showtext)
-showtext.auto()
+showtext_opts(dpi=200)
+showtext_auto()
 ggplot(cars)+
     geom_point(aes(x=speed, y=dist))+
-    theme_ofce(base_family = "Open Sans", base_size = 48)
+    theme_ofce(base_family = "Open Sans", base_size = 12)
 
