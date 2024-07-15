11f43ee082357c12000b6d93fb46a19af1af643d
PaulMalliet <paul.malliet@sciencespo.fr>
2024-07-15 17:09:06 +0200

    Ajout palette OFCE (version Juin 2024) dans data du package

---
+++ R/doc_palette_principale.R
@@ -0,0 +1,13 @@
+#' palette_principale
+#'
+#' Description.
+#'
+#' @format A data frame with 8 rows and 4 variables:
+#' \describe{
+#'   \item{ Nom }{  numeric }
+#'   \item{ Code }{  character }
+#'   \item{ R-V-B }{  character }
+#'   \item{ Observations }{  character }
+#' }
+#' @source Source
+"palette_principale"
