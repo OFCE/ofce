4c0e4b73e94c021b6d10852403a15059d5c753b5
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2024-10-06 18:21:25 +0200

    scale date

--- R/scale_color_pays.R
+++ R/scale_color_pays.R
@@ -3,49 +3,39 @@
 #'
 #' @param lang chr, le choix de la langue c("fr","en")
 #' @param list_iso3 chr, une liste de nom de pays
+#' @param name titre de la légende
+#' @param aesthetics couleur ou fill ou les deux
+#' @param ... passé à scale_colour_manual
 #'
 #' @importFrom dplyr filter
 #' @importFrom dplyr arrange
 #' @importFrom dplyr mutate
-#' @importFrom purrr set_names
 #'
-#' @return une palette de couleur (format HEX)
+#' @return un scale configuré
 #' @export
 #'
 #'
-#'
-scale_color_pays <- function(list_iso3 = NULL,
-                             lang = NULL) {
-
-  # load("data/palette_pays.rda")
-
-  if (is.null(list_iso3)){list_iso3 = c("FRA", "ITA")}
-  if (is.null(lang)){lang = "fr"}
+scale_color_pays <- function(list_iso3 = c("FRA", "ITA", "ESP", "DEU"),
+                             lang = "fr", name = NULL, aesthetics= c("color", "fill"), ...) {
 
   dat <- ofce::palette_pays |>
     dplyr::filter(ISO3 %in% list_iso3) |>
     dplyr::arrange(match(.data[["ISO3"]], list_iso3))
 
   if(lang == "fr"){
-
     dat <- dat |> dplyr::mutate(label = .data[["label_fr"]])} else {
-
       if(lang == "en"){
-
         dat <- dat |> dplyr::mutate(label = .data[["label_en"]])} else {
-
           dat <- dat |> dplyr::mutate(label = .data[["ISO3"]])
-
           cat("Il n'existe pas de traduction pour la langue demandée. Seuls le français (\"fr\") et l'anglais (\"en\") sont pour l'instant proposés.")
           }
-
-
     }
 
   scale_colour_manual(values = dat$HEX,
                       breaks = dat$ISO3,
-                      labels = dat$label)
-
-
+                      labels = dat$label,
+                      name = name,
+                      aesthetics = aesthetics,
+                      ...)
 }
 
---
+++ R/scale_ofce_date.R
@@ -0,0 +1,24 @@
+
+#' Echelle pour les x du format habituel des grahiques à date
+#'
+#' @param date liste de date
+#' @param freq soit day, month, quarter, year
+#' @param lang langue de retour
+#' @return chaine de charactères
+#' @export
+
+scale_ofce_date <- function(date_breaks = "5 years",
+                            date_minor_breaks = "1 year",
+                            date_labels = "%Y",
+                            guide = "minor_breaks",
+                            name = NULL,
+                            ...) {
+  list(
+    scale_x_date(date_breaks = date_breaks,
+               date_minor_breaks = date_minor_breaks,
+               date_labels = date_labels,
+               guide = guide,
+               name = name,
+               ...),
+    guides(x = guide_axis(minor.ticks = TRUE)))
+}
