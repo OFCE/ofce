7f8a4f034ddadc698cbe568c0c5a039a35e7156f
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2024-10-07 10:23:37 +0200

    aide

--- R/ofce_caption.R
+++ R/ofce_caption.R
@@ -3,18 +3,18 @@
 #' permet de construire un caption facilement avec un wrapping
 #' on commence par la source, une note, et une lecture
 #'
-#' @param source texte de la source (sans source)
-#' @param note texte de la note
-#' @param lecture texte de la note de lecture
+#' @param source texte de la source (sans le mot source qui est rajouté)
+#' @param note texte de la note (sans le mot note qui est rajouté)
+#' @param lecture texte de la note de lecture (sans le mot lecture qui est rajouté)
 #' @param dpt dernier point connu
 #' @param dptf fréquence du dernier point connu (day, month, quarter, year)
-#' @param wrap largeur du texte
-#' @param ofce (bool) si TRUE ajoute calculs OFCE
-#' @param lang langue des textes
-#' @return ggplot2 caption (ggplot() + ofce_caption())
+#' @param wrap largeur du texte en charactères (120 charactères par défaut)
+#' @param ofce (bool) si TRUE ajoute calculs OFCE à source, sinon rien, TRUE par défaut
+#' @param lang langue des textes (fr par défaut)
+#' @return ggplot2 caption (ggplot() + ofce_caption("INSEE"))
 #' @export
 
-ofce_caption <- function(source = "Calculs OFCE",
+ofce_caption <- function(source = NULL,
                          note = NULL,
                          lecture = NULL,
                          dpt = NULL,
@@ -24,51 +24,58 @@ ofce_caption <- function(source = "Calculs OFCE",
     lec <- "*Lecture* : "
     src <- "*Source* : "
     not <- "*Note* : "
-    ofc <- "Calculs OFCE"
+    Ofc <- "Calculs OFCE"
+    ofc <- ", calculs OFCE"
     der <- ", dernier point connu : "
     Der <- "*Dernier point connu* : "}
   else {
     lec <- "*Reading*: "
     src <- "*Source*: "
     not <- "*Note*: "
-    ofc <- "Computation by OFCE"
+    ofc <- "OFCE' computation"
+    Ofc <- ofc
     der <- ", last known point: "
     Der <- "*Last known point*: "
   }
   caption <- ""
 
-  if(!is.null(lecture)) {
+  if(length(lecture)>0) {
     caption <- str_c(lec, lecture)  |>
+      str_c(".") |>
       str_wrap(width = wrap) |>
       str_replace_all("\\n", "<br>")
   }
 
-  if(!is.null(note)) {
+  if(length(note)>0) {
     if(length(caption>0))
       caption <- caption |> str_c("<br>")
     addcaption <- str_c(not, note) |>
-      str_wrap(width = wrap, exdent = 2) |>
+      str_c(".") |>
+      str_wrap(width = wrap) |>
       str_replace_all("\\n", "<br>")
     caption <- caption |>
       str_c(addcaption)
   }
   if(ofce) {
-    source <- str_c(source , ofc, sep = ", ")
+    if(length(source)==0)
+      source <- Ofc else
+        source <- str_c(source , ofc)
   }
 
-  if(!is.null(source)) {
+  if(length(source)>0) {
     if(length(caption>0))
       caption <- caption |> str_c("<br>")
     if(str_detect(source, ",|;"))
       src <- src |> str_replace("ce", "ces")
     addcaption <- str_c(src, source) |>
+      str_c(".") |>
       str_wrap(width = wrap) |>
       str_replace_all("\\n", "<br>")
     caption <- caption |>
       str_c(addcaption)
   }
 
-  if(!is.null(dpt)) {
+  if(length(dpt)>0) {
     if(length(caption>0))
         caption <- caption |> str_c("<br>", Der, dernier_point(dpt, dptf, lang)) else
           caption <- caption |> str_c(Der, dernier_point(dpt, dptf, lang))
