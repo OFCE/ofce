7edece8c74cd189bdeb9242be38161586e8659f6
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2025-02-06 08:00:13 +0100

    Update theme_ofce_1.R

--- R/theme_ofce_1.R
+++ R/theme_ofce_1.R
@@ -212,46 +212,86 @@ theme_ofce_void <- function(base_size = getOption("ofce.base_size"),
         style = marquee::modify_style(ofce_style, tag = "base", weight = "bold"),
         hjust = 0,
         vjust = 0.5,
-        margin = ggplot2::margin(b=-6, t=0),
+        margin = ggplot2::margin(),
         lineheight = 1),
+      axis.title = marquee::element_marquee(
+        style = marquee::modify_style(ofce_style, tag = "base", weight = "normal"),
+        size = ggplot2::rel(0.85),
+        hjust = 1,
+        vjust = 0.5,
+        margin = ggplot2::margin(t=12, l=12),
+        lineheight = 1),
+      axis.text = marquee::element_marquee(
+        style = marquee::modify_style(ofce_style, tag = "base", weight = "normal"),
+        size = ggplot2::rel(0.85),
+        colour = "gray25",
+        lineheight = 1),
+      axis.text.y = marquee::element_marquee(
+        hjust = 1,
+        vjust=0),
+      axis.text.x = marquee::element_marquee(
+        margin = ggplot2::margin(t=3),
+        hjust = 0.5),
       plot.subtitle = marquee::element_marquee(
         style = ofce_style,
-        size = ggplot2::rel(0.6),
+        size = ggplot2::rel(0.85),
         hjust = 0,
-        margin = ggplot2::margin(b=0, t=-6),
+        margin = ggplot2::margin(),
         lineheight = 1),
       plot.caption = marquee::element_marquee(
         style = marquee::modify_style(
           ofce_style, tag = "p",
           margin = marquee::trbl(2, 0, 0, 0)),
-        size = ggplot2::rel(0.75),
+        size = ggplot2::rel(0.85),
         hjust = 0,
-        margin = margin(l = 0, t = 0),
-        width = 0.8),
+        margin = ggplot2::margin(t=6,b=6),
+        width = 0.99),
       strip.text = marquee::element_marquee(
         style = ofce_style,
-        size = ggplot2::rel(0.85),
+        size = ggplot2::rel(0.9),
         hjust = 0.5,
         vjust = 0.5,
-        margin = margin(t=6, b=6)))
+        margin = ggplot2::margin(t=6,b=6)),
+      legend.title = marquee::element_marquee(
+        size = ggplot2::rel(0.85),
+        color = "gray25",
+        margin = ggplot2::margin(r=12, b=6, t=6, l=6)),
+      legend.text = ggplot2::element_text(
+        size = ggplot2::rel(0.5),
+        hjust=0,
+        color = "gray25")
+    )
   else
     ggplot2::theme(
-      legend.text = ggplot2::element_text(
-        size = ggplot2::rel(0.75),
-        color = "gray25"),
       plot.title = ggtext::element_markdown(
         face = "bold",
         hjust = 0,
         vjust = 0.5,
         margin = ggplot2::margin(b=0, t=0),
         lineheight = 1.25),
-      plot.subtitle = ggplot2::element_text(
-        size = ggplot2::rel(0.60),
+      axis.title  = ggplot2::element_text(
+        size = ggplot2::rel(0.75),
+        color = "#222222",
+        margin = margin(t = 0, b=0, l= 0, r=0),
+        hjust = 1),
+      axis.text = ggplot2::element_text(
+        family = base_family,
+        size = ggplot2::rel(0.75)),
+      axis.text.y = ggplot2::element_text(
+        color = "gray25",
+        hjust = 1,
+        vjust=0),
+      axis.text.x = ggplot2::element_text(
+        color = "gray25",
+        margin=margin(t=6, b=0),
+        hjust = 0.5),
+      plot.subtitle = ggtext::element_markdown(
+        size = ggplot2::rel(0.85),
         hjust = 0,
         margin = ggplot2::margin(b=0, t=0),
         lineheight = 1.25),
       plot.caption = ggtext::element_markdown(
-        size = ggplot2::rel(0.80),
+        size = ggplot2::rel(0.85),
         hjust = 0,
         margin = ggplot2::margin(l = 0, t = 6),
         lineheight = 1.25),
@@ -259,7 +299,14 @@ theme_ofce_void <- function(base_size = getOption("ofce.base_size"),
         size = ggplot2::rel(0.9),
         hjust = 0.5,
         vjust = 0.5,
-        margin = ggplot2::margin(t=6, b=6)))
+        margin = ggplot2::margin(t=6, b=6)),
+      legend.text = ggplot2::element_text(
+        size = ggplot2::rel(0.75),
+        color = "gray25"),
+      legend.title = ggplot2::element_text(
+        size = ggplot2::rel(0.80),
+        color = "gray25")
+    )
 
   theme_foundation() +
     ggplot2::theme(
