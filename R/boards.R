e8e86f610bca804d11a8a6ef49bd00697318a809
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2025-02-24 15:00:36 +0100

    Update boards.R

--- R/boards.R
+++ R/boards.R
@@ -30,7 +30,7 @@ bd_hash <- function(obj) pins::pin_meta(board, obj)$pin_hash
 #' @export
 #'
 bd_read <- function(obj) {
-  pins::pin_read(board, obj)
+  pins::pin_read(ofce::board(), obj)
 }
 
 #' Ecrit sur le board
@@ -53,7 +53,7 @@ bd_read <- function(obj) {
 bd_write <- function(obj, name=NULL, title=NULL, description=NULL, metadata = NULL, tags=NULL, versioned=NULL) {
   if(is.null(name))
     name <- rlang::as_name(rlang::enquo(obj))
-  pins::pin_write(board = board(),
+  pins::pin_write(board = ofce::board(),
                   x = obj,
                   name = name,
                   title = title,
