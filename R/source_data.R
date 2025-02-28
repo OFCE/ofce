efc8957fe2a769acacecba3d1b6a20ff578eca8b
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2025-02-28 08:58:43 +0100

    update

--- R/source_data.R
+++ R/source_data.R
@@ -1,19 +1,34 @@
 
-# source_data, aliases pour comptabilité avec le changement de nom ------------------------------
+#' @export
+#' @importFrom sourcoise sourcoise
+sourcoise::sourcoise
+
+#' @export
+#' @importFrom sourcoise sourcoise_refresh
+sourcoise::sourcoise_refresh
+
+#' @export
+#' @importFrom sourcoise sourcoise_status
+sourcoise::sourcoise_status
+
+#' @export
+#' @importFrom sourcoise sourcoise_clear
+sourcoise::sourcoise_clear
+
+# source_data, aliases pour compatibilité avec le changement de nom ------------------------------
 
 #' @export
 #' @rdname sourcoise
-
-source_data <- sourcoise::sourcoise
+source_data <- sourcoise
 
 #' @export
 #' @rdname sourcoise_refresh
-source_data_refresh <- sourcoise::sourcoise_refresh
+source_data_refresh <- sourcoise_refresh
 
 #' @export
 #' @rdname sourcoise_status
-source_data_status <- sourcoise::sourcoise_status
+source_data_status <- sourcoise_status
 
 #' @export
 #' @rdname sourcoise_clear
-source_data_clear <- sourcoise::sourcoise_clear
+source_data_clear <- sourcoise_clear
