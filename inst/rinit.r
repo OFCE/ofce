9243f6e5d852a7831d384e1ad2f509a40af62c8f
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2025-10-29 09:19:35 +0100

    Update rinit.r

--- inst/rinit.r
+++ inst/rinit.r
@@ -14,7 +14,7 @@ opts_chunk$set(
 ## Checking rinit packages installation
 rinit_packages <- c("pak",
                     "tidyverse",
-                    # "ofce",
+                    "yaml",
                     "showtext",
                     "plotly",
                     "gt",
@@ -34,7 +34,8 @@ rinit_packages <- c("pak",
                     "cli",
                     "quarto",
                     "gdtools",
-                    "gfonts"
+                    "gfonts",
+                    "marquee"
                     )
 
 ## Installation of missing packages
@@ -54,7 +55,9 @@ options(
   ofce.caption.ofce = FALSE,
   ofce.marquee = TRUE,
   ofce.caption.srcplus = NULL,
-  ofce.caption.wrap = 0)
+  ofce.caption.wrap = 0,
+  sourcoise.init_fn = ofce::init_qmd,
+  sourcoise.grow_cache = Inf)
 showtext_opts(dpi = 120)
 showtext_auto()
 
@@ -71,6 +74,8 @@ tooltip_css  <-
   r:20px;"
 
 gdtools::register_gfont("Open Sans")
+
+set_theme(theme_ofce())
 
 margin_download <- function(data, output_name = "donnees", label = "données") {
   if(knitr::is_html_output()) {
