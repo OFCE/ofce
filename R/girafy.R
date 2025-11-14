f4dd4a4ca926c84fc450cbe23de5f8433a33c4ca
Xavier Timbeau <xavier.timbeau@sciencespo.fr>
2025-11-14 15:47:28 +0100

    girafy

--- R/girafy.R
+++ R/girafy.R
@@ -16,7 +16,7 @@
 #' @returns un ggplot ou un objet ggiraph
 #' @export
 
-girafy <- function(plot, r=2.5, o = 0.5, out=NULL, id = NULL,  ...) {
+girafy <- function(plot, r=2.5, o = 0.5, out=getOption("ofce.savegraph"), id = NULL,  ...) {
   if(!is.null(out))
     ofce::savegraph(plot, id=id)
   if(knitr::is_html_output()| interactive()) {
@@ -43,6 +43,18 @@ girafe_opts <- function(x, ...) ggiraph::girafe_options(
   ggiraph::opts_tooltip(css = tooltip_css, delay_mouseover = 10, delay_mouseout = 3000)) |>
   ggiraph::girafe_options(...)
 
+#' Enregistre un graphe
+#'
+#' @param graph le ggplot
+#' @param label son label (mis à partir de knitr si possible)
+#' @param chunk les infos de chunk (de knitr)
+#' @param document le nom du document qmd (à partir de quarto)
+#' @param id un id pour les tabset
+#' @param dest le dossier de sauvegarde (ofce.savegraph)
+#'
+#' @returns le graphique, avec un effet de bord qui est le ggplot enregistré
+#' @export
+#'
 savegraph <- function(graph, label=NULL,
                       chunk = knitr::opts_current$get(),
                       document=knitr::current_input(),
@@ -91,4 +103,24 @@ savegraph <- function(graph, label=NULL,
   qs2::qs_save(object = graph, file = str_c(fn, ".ggplot"))
 
   return(graph)
+}
+
+#' Lit un graphique sauvegardé
+#'
+#' Lorsque `ofce.savegraph` ets non NULL, chaque graphique qui passe dans `girafy` est enregistré dans le dossier `ofce.savegraph`.
+#' Son nom est formé en ajoutant le dossier qui le contient, le nom du fichier .qmd, le label du fichier (séparé par des tirets)
+#' par exmple : "index-fig-psal" ou "france-synthese-fig-indicateurs"
+#'
+#' @param graphe (string) nom du graphique composé du dossier d'un tiret du nom du document d'un tiret et du label du graphique
+#'
+#' @returns un ggplot
+#' @export
+#'
+load_graphe <- function(graphe) {
+  dir <- optionGet("ofce.savegraph")
+  if(is.null(dir))
+    return(NULL)
+  qs2::qs_read(
+    fs::path_join(c(ofce.project.root, optionGet(ofce.savegraph), graphe)) |>
+      fs::path_ext_set("ggplot"))
 }
